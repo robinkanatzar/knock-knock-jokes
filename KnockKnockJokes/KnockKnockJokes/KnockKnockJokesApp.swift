c5a62e3b7b47ef06743c2d4bc24a5281ada4d7c9
Robin Kanatzar <>
2025-07-16 16:56:43 +0200

    Change from blue buttons to yellow highlight style

---
+++ KnockKnockJokes/KnockKnockJokes/KnockKnockJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnockKnockJokesApp.swift
+//  KnockKnockJokes
+//
+//  Created by Robin Kanatzar on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct KnockKnockJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
