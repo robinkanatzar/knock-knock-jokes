ff49cc773df2d3099ed2dbf7a69db4519df5227e
Robin Kanatzar <>
2025-09-29 12:16:06 +0200

    Home view in scroll view

--- ios/Targets/App/Sources/View/HomeView.swift
+++ ios/Targets/App/Sources/View/HomeView.swift
@@ -10,49 +10,57 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading, spacing: 20) {
-                NavigationLink {
-                    Example1View()
-                } label: {
-                    HStack {
-                        Text("Example 1: Utterance")
-                        Spacer()
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    NavigationLink {
+                        Example1View()
+                    } label: {
+                        HStack {
+                            Text("Example 1: Utterance")
+                            Spacer()
+                        }
                     }
+                    NavigationLink {
+                        Example2View()
+                    } label: {
+                        Text("Example 2: Utterance \"Knock Knock\"")
+                    }
+                    NavigationLink {
+                        Example3View()
+                    } label: {
+                        Text("Example 3: .duckOthers")
+                    }
+                    NavigationLink {
+                        Example4View()
+                    } label: {
+                        Text("Example 4: .mixWithOthers")
+                    }
+                    NavigationLink {
+                        Example5View()
+                    } label: {
+                        Text("Example 5: Is VoiceOver on?")
+                    }
+                    NavigationLink {
+                        Example6View()
+                    } label: {
+                        Text("Example 6: Joke .mixWithOthers")
+                    }
+                    NavigationLink {
+                        Example7View()
+                    } label: {
+                        Text("Example 7: Joke When VoiceOver Off")
+                    }
+                    Divider()
+                    NavigationLink {
+                        SelfTalkView()
+                    } label: {
+                        Text("Self Talk View")
+                    }
+                    
+                    Spacer()
                 }
-                NavigationLink {
-                    Example2View()
-                } label: {
-                    Text("Example 2: Utterance \"Knock Knock\"")
-                }
-                NavigationLink {
-                    Example3View()
-                } label: {
-                    Text("Example 3: .duckOthers")
-                }
-                NavigationLink {
-                    Example4View()
-                } label: {
-                    Text("Example 4: .mixWithOthers")
-                }
-                NavigationLink {
-                    Example5View()
-                } label: {
-                    Text("Example 5: Is VoiceOver on?")
-                }
-                NavigationLink {
-                    Example6View()
-                } label: {
-                    Text("Example 6: Joke .mixWithOthers")
-                }
-                NavigationLink {
-                    Example7View()
-                } label: {
-                    Text("Example 7: Joke When VoiceOver Off")
-                }
-                
-                Spacer()
+                .padding()
             }
-            .padding()
             .navigationTitle("When iOS Talks Back")
         }
     }
